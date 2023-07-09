c3acaeba042ba55c7234fb15938329bb562c548c
developer-sujon <developersujon@yahoo.com>
2023-07-09 12:07:11 +0600

    Custom Exception Filters | Catch-All Exception Filter | Handle any Exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_PIPE, APP_FILTER } from '@nestjs/core';
 import { TasksModule } from './tasks/tasks.module';
+import { AppExceptionFilter } from './exceptions/app-exception.filter';
 
 @Module({
   imports: [TasksModule],
   controllers: [],
-  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    // marking `AppExceptionFilter` global
+    // we can also declare it in `main.ts` but then DI (Dependency Injection) will not work
+    // and we need to pass the HttpAdapterHost explicitly in the `main.ts`
+    { provide: APP_FILTER, useClass: AppExceptionFilter },
+  ],
 })
 export class AppModule {}
